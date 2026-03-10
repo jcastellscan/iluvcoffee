d60f59a7a91eb317c240619b59838d57797c95f4
jcastellscan <jcastellscan@gmail.com>
2026-03-10 23:55:57 +0100

    add pagination

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,26 @@
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
+
+@Entity() // sql table === 'coffee'
 export class Coffee {
+  @PrimaryGeneratedColumn() // primary key with auto increment
   id: number;
+  @Column()
   name: string;
+  @Column()
   brand: string;
-  flavors: string[];
+
+  @JoinTable() // 👈 Join the 2 tables - only the OWNER-side does this
+  @ManyToMany(
+    (type) => Flavor,
+    (flavor) => flavor.coffees, // what is "coffee" within the Flavor Entity
+    { cascade: true }, // 👈 allows us to create a new flavor on the fly when we create a new coffee
+  ) // 👈
+  flavors: Flavor[];
 }
