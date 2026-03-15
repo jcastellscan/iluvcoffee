089b6434d2fda243ab579117267c198cc3432dff
jcastellscan <jcastellscan@gmail.com>
2026-03-15 01:46:45 +0100

    test custom providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,10 +4,25 @@ import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
+
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
+    { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
+    CoffeesService,
+  ],
 })
 export class CoffeesModule {}
